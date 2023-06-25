0c2af54a659a313de8026429113aad3ee78d489c
salysaly <anyawee.sr@gmail.com>
2023-06-25 22:58:58 +0700

    artwork page (calculate ratio)

--- src/pages/work/artwork.jsx
+++ src/pages/work/artwork.jsx
@@ -2,39 +2,92 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import React, { useState, useMemo } from 'react';
+
+const ImageRow = ({ mediaArtworks }) => {
+	const [imageData, setImageData] = useState([]);
+	const onLoadImage = (e, index) => {
+		const newWidth = (e.target.width * 500) / e.target.height;
+		const newImageData = [...imageData];
+		newImageData[index] = newWidth;
+		setImageData(newImageData);
+	};
+	const calculatedWidthPercent = useMemo(() => {
+		// (partialSum, a) => partialSum + a, 0 อันนี้เขียนแบบ short return
+		const sum = imageData.reduce((partialSum, a) => partialSum + a, 0);
+		// สามาดเขียน return imageData.map((data) => (data * 100) / sum) ก็ได้เพราะไม่จำเป็นต้องสร้างตัวแปรเพื่อ return
+		const newCalculated = imageData.map((data) => {
+			return (data * 100) / sum;
+		});
+		return newCalculated;
+	}, [imageData]);
+
+	return (
+		<div className={styles.gifContainer}>
+			{mediaArtworks.map((media, index) => (
+				<Image
+					key={media.url}
+					width={0}
+					height={0}
+					style={{
+						// width: calculatedWidthPercent?.[index]
+						// 	? `${calculatedWidthPercent?.[index]}%`
+						// 	: '100%',
+						height: 'auto',
+						flex: `${
+							calculatedWidthPercent?.[index]
+								? `${calculatedWidthPercent[index]}%`
+								: '100%'
+						} 1 1`,
+					}}
+					src={media.url}
+					alt="artwork"
+					// สร้าง fn มาครอบเพื่อรับ parameter จาก callback แล้วส่งไป fn ข้างบนอีกที
+					onLoad={(event) => onLoadImage(event, index)}
+				/>
+			))}
+		</div>
+	);
+};
 
 function Home() {
 	const artwork = [
-		{
-			name: 'artwork-1',
-			media: [
-				{
-					url: '/images/artwork/artwork_141.gif',
-					type: 'image',
-					width: '1020',
-					height: '580',
-				},
-			],
-			type: 'image',
-		},
-		{
-			name: 'artwork-2',
-			media: [
-				{
-					url: '/images/artwork/artwork_4.gif',
-					type: 'image',
-					width: '1824',
-					height: '1026',
-				},
-				{
-					url: '/images/artwork/artwork_95.gif',
-					type: 'image',
-					width: '1344',
-					height: '756',
-				},
-			],
-			type: 'image',
-		},
+		[
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_141.gif',
+			},
+		],
+		[
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_4.gif',
+			},
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_95.gif',
+			},
+		],
+		[
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_5.gif',
+			},
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_137.gif',
+			},
+		],
+		[
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_5-2.gif',
+			},
+			{
+				type: 'image',
+				url: '/images/artwork/artwork_5-3.gif',
+			},
+		],
 	];
 
 	return (
@@ -65,209 +118,9 @@ function Home() {
 				</div>
 			</header>
 			<main className={styles.pageArtwork}>
-				{/* {artwork.map((row) => (
-					<div key={`container-${row.name}`} className={styles.videoContainer}>
-						{row.media.map((media) => (
-							<div
-								key={`items-${media.url}`}
-								className={
-									media.type === 'iframe'
-										? styles.videoColumn
-										: styles.gifColumn
-								}
-							>
-								{media.type === 'iframe' ? (
-									<div className={styles.videoItem}>
-										<iframe
-											src={media.url}
-											title="YouTube video player"
-											frameBorder="0"
-											allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-											allowFullScreen
-										></iframe>
-									</div>
-								) : (
-									<Image
-										className={styles.gifImage}
-										width={media.width}
-										height={media.height}
-										src={media.url}
-										alt="artwork"
-									/>
-								)}
-							</div>
-						))}
-					</div>
-				))} */}
-
-				{/* type in Lv.1 */}
-				{/* {artwork.map((row) => (
-					<div
-						key={`testContainer-${row.name}`}
-						className={
-							row.type === 'iframe'
-								? styles.videoContainer
-								: styles.gifContainer
-						}
-					>
-						{row.media.map((media) => (
-							<div
-								key={`testColumn-${media.url}`}
-								className={
-									row.type === 'iframe' ? styles.videoColumn : styles.gifColumn
-								}
-							>
-								{row.type === 'iframe' ? (
-									<div className={styles.videoItem}>
-										<iframe
-											src={media.url}
-											title="YouTube video player"
-											frameBorder="0"
-											allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-											allowFullScreen
-										></iframe>
-									</div>
-								) : (
-									<Image
-										className={styles.gifImage}
-										width={media.width}
-										height={media.height}
-										src={media.url}
-										alt="artwork"
-									/>
-								)}
-							</div>
-						))}
-					</div>
-				))} */}
-
-				{/* example */}
-				<div className={styles.flexbox}>
-					<div className={styles.item3}>AAA</div>
-					<div className={styles.item4}>BBB</div>
-				</div>
-				{/* iframe */}
-				<div className={styles.videoContainer}>
-					<div className={styles.videoColumn}>
-						<div className={styles.videoItem}>
-							<iframe
-								src="https://www.youtube.com/embed/5M9IERFJo3k"
-								title="YouTube video player"
-								frameBorder="0"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-								allowFullScreen
-							></iframe>
-						</div>
-					</div>
-					<div className={styles.videoColumn}>
-						<div className={styles.videoItem}>
-							<iframe
-								src="https://www.youtube.com/embed/5M9IERFJo3k"
-								title="YouTube video player"
-								frameBorder="0"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-								allowFullScreen
-							></iframe>
-						</div>
-					</div>
-				</div>
-
-				{/* gif */}
-				<div className={styles.videoContainer}>
-					<div className={styles.gifContainer}>
-						<div className={styles.gifItemAA}>
-							<Image
-								className={styles.gifImage}
-								width={1020}
-								height={580}
-								src="/images/artwork/artwork_141.gif"
-								alt="artwork"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className={styles.videoContainer}>
-					<div className={styles.gifContainer}>
-						<div className={styles.gifItemAA}>
-							<Image
-								className={styles.gifImage}
-								width={1824}
-								height={1026}
-								src="/images/artwork/artwork_4.gif"
-								alt="artwork"
-							/>
-						</div>
-						<div className={styles.gifItemAA}>
-							<Image
-								className={styles.gifImage}
-								width={1824}
-								height={1026}
-								src="/images/artwork/artwork_95.gif"
-								alt="artwork"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className={styles.gifContainer}>
-					<div className={styles.gifItemAA}>
-						<Image
-							className={styles.gifImage}
-							width={960}
-							height={540}
-							src="/images/artwork/artwork_5.gif"
-							alt="artwork"
-						/>
-					</div>
-					<div className={styles.gifItemAA}>
-						<Image
-							className={styles.gifImage}
-							width={650}
-							height={650}
-							src="/images/artwork/artwork_137.gif"
-							alt="artwork"
-						/>
-					</div>
-				</div>
-				<div className={styles.gifContainer}>
-					<div className={styles.gifItemAA}>
-						<Image
-							className={styles.gifImage}
-							width={1536}
-							height={864}
-							src="/images/artwork/artwork_5-2.gif"
-							alt="artwork"
-						/>
-					</div>
-					<div className={styles.gifItemAA}>
-						<Image
-							className={styles.gifImage}
-							width={960}
-							height={540}
-							src="/images/artwork/artwork_5-3.gif"
-							alt="artwork"
-						/>
-					</div>
-				</div>
-				<div className={styles.gifContainer}>
-					<div className={styles.gifItem}>
-						<Image
-							className={styles.gifImage}
-							width={1176}
-							height={1080}
-							src="/images/artwork/artwork_sara.gif"
-							alt="artwork"
-						/>
-					</div>
-					<div className={styles.gifItem}>
-						<Image
-							className={styles.gifImage}
-							width={864}
-							height={486}
-							src="/images/artwork/artwork_135.gif"
-							alt="artwork"
-						/>
-					</div>
-				</div>
+				{artwork.map((row, index) => (
+					<ImageRow key={index} mediaArtworks={row} />
+				))}
 			</main>
 		</div>
 	);
