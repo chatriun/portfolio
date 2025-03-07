023ac778b2321bd5c62db03631ee3988cf4d13a8
salysaly <anyawee.sr@gmail.com>
2025-03-07 21:38:32 +0700

    feat(artwork): use .mp4 instead .gif, create VideoElement component, update ImageRow component

--- src/components/ImageRow.jsx
+++ src/components/ImageRow.jsx
@@ -1,15 +1,19 @@
 import { useMemo, useState } from 'react';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
+import VideoElement from './VideoElement';
 
 const ImageRow = ({ mediaArtworks }) => {
 	const [imageData, setImageData] = useState([]);
 
 	const onLoadImage = (e, index) => {
 		const newWidth = (e.target.offsetWidth * 500) / e.target.offsetHeight;
-		const newImageData = [...imageData];
-		newImageData[index] = newWidth;
-		setImageData(newImageData);
+		setImageData((prevState) => {
+			const newImageData = [...prevState];
+			newImageData[index] = newWidth;
+
+			return newImageData;
+		});
 	};
 
 	const calculatedWidthPercent = useMemo(() => {
@@ -17,62 +21,91 @@ const ImageRow = ({ mediaArtworks }) => {
 		const newCalculated = imageData.map((data) => {
 			return (data * 100) / sum;
 		});
+
 		return newCalculated;
 	}, [imageData]);
 
 	return (
 		<div className={styles.gifContainer}>
-			{mediaArtworks.map((media, index) =>
-				media.type === 'image' ? (
-					<Image
-						key={media.url}
-						width={0}
-						height={0}
-						style={{
-							height: 'auto',
-							flex: `${
-								calculatedWidthPercent?.[index]
-									? `${calculatedWidthPercent[index]}%`
-									: '100%'
-							} 1 1`,
-						}}
-						src={media.url}
-						alt="artwork"
-						// สร้าง fn มาครอบเพื่อรับ parameter จาก callback แล้วส่งไป fn ข้างบนอีกที
-						onLoad={(event) => onLoadImage(event, index)}
-					/>
-				) : (
-					<div
-						key={media.url}
-						style={{
-							height: 'auto',
-							flex: `${
-								calculatedWidthPercent?.[index]
-									? `${calculatedWidthPercent[index]}%`
-									: '100%'
-							} 1 1`,
-						}}
-					>
+			{mediaArtworks.map((media, index) => {
+				if (media.type === 'image') {
+					return (
+						<Image
+							key={media.url}
+							width={0}
+							height={0}
+							style={{
+								height: 'auto',
+								flex: `${
+									calculatedWidthPercent?.[index]
+										? `${calculatedWidthPercent[index]}%`
+										: '100%'
+								} 1 1`,
+							}}
+							src={media.url}
+							alt="artwork"
+							// สร้าง fn มาครอบเพื่อรับ parameter จาก callback แล้วส่งไป fn ข้างบนอีกที
+							onLoad={(event) => onLoadImage(event, index)}
+						/>
+					);
+				} else if (media.type === 'vdo') {
+					return (
 						<div
-							className={styles.videoItem}
-							style={
-								media.width &&
-								media.height && {
-									paddingBottom: `${(media.height * 100) / media.width}%`,
-								}
-							}
+							key={media.url}
+							style={{
+								height: 'auto',
+								flex: `${
+									calculatedWidthPercent?.[index]
+										? `${calculatedWidthPercent[index]}%`
+										: '100%'
+								} 1 1`,
+							}}
 						>
-							<iframe
-								onLoad={(event) => onLoadImage(event, index)}
-								src={media.url}
-								title="YouTube video player"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-								allowFullScreen
+							<VideoElement
+								url={media.url}
+								index={index}
+								onSetImageData={setImageData}
 							/>
 						</div>
-					</div>
-				)
-			)}
+					);
+				} else if (media.type === 'iframe') {
+					return (
+						<div
+							key={media.url}
+							style={{
+								height: 'auto',
+								flex: `${
+									calculatedWidthPercent?.[index]
+										? `${calculatedWidthPercent[index]}%`
+										: '100%'
+								} 1 1`,
+							}}
+						>
+							<div
+								className={styles.videoItem}
+								style={
+									media.width &&
+									media.height && {
+										paddingBottom: `${(media.height * 100) / media.width}%`,
+									}
+								}
+							>
+								<iframe
+									onLoad={(event) => onLoadImage(event, index)}
+									src={media.url}
+									title="YouTube video player"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+									allowFullScreen
+								/>
+							</div>
+							{/* TODO: use component LinkYoutube */}
+							{/* <LinkYoutube workItem={media.url} /> */}
+						</div>
+					);
+				} else {
+					return null;
+				}
+			})}
 		</div>
 	);
 };
